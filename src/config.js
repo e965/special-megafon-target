3a23129a0e9e9f50bfbbae90bde9544460b9e48c
Dmitry <e965@protonmail.com>
2019-05-28 02:26:24 +0500

    1.0.1

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-  name: 'Special', // уникальное имя спецпроекта. Оно же — название главного класса. Используется на странице, куда интегрируется спецпроект
-  analyticsCategory: 'Category Name',
+  name: 'MegafonTarget', // уникальное имя спецпроекта. Оно же — название главного класса. Используется на странице, куда интегрируется спецпроект
+  analyticsCategory: 'Megafon Target',
   sendPageView: false, // отключаем, если спецпроект не на отдельной странице
   listenedEvents: ['click'] // слушаем события (click, input, change, etc.). Обычно нужен только click
 };
